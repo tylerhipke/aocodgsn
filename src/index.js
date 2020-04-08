9f3e592e235b28cb87345aa849e5412cf1b69f05
tylerhipke <tylerhipke@gmail.com>
2020-04-07 20:56:49 -0700

    change folder structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from './components/NavBar.js'
 
-ReactDOM.render((
-    <Router history = {browserHistory}>
-       <Route path = "/" component = {App}>
-          <IndexRoute component = {Home} />
-          <Route path = "home" component = {Home} />
-          <Route path = "about" component = {About} />
-          <Route path = "contact" component = {Contact} />
-       </Route>
-    </Router>
- ), document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <Route path="/" component={ NavBar }/>
+    </BrowserRouter>, document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
